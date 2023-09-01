2a0bdfe073bc0a7f57fefcaaba28da56c97dbd3d
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-09-01 19:23:55 -0300

    posta tweet

--- Blog/bot_twitter.py
+++ Blog/bot_twitter.py
@@ -1,17 +1,10 @@
 import tweepy
 from decouple import config
 
-API_KEY = config('API_Key')
-API_SECRET_KEY = config('API_Key_Secret')
-BEARER_TOKEN = config('bearer_token')
-
-auth = tweepy.AppAuthHandler(API_KEY, API_SECRET_KEY)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-
-query = 'livros'
-tweets = api
-
-for tweet in tweets:
-    print("Username:", tweet.user.screen_name)
-    print("Tweet:", tweet.full_text)
+client = tweepy.Client(
+    consumer_key=config('API_Key'),
+    consumer_secret=config('API_Key_Secret'),
+    access_token=config('Access_Token'),
+    access_token_secret=config('Access_Token_Secret')
+)
+client.create_tweet(text="Test tweet!")
