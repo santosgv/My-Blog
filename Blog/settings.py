78887df70856ab5ea9e55536f17ed091a14b182c
vitor <santosgomesv@gmail.com>
2023-09-07 23:39:21 -0300

    tegs

--- Blog/settings.py
+++ Blog/settings.py
@@ -16,6 +16,12 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
+if not DEBUG:
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	SECURE_SSL_REDIRECT = True
+	SESSION_COOKIE_SECURE = True
+	CSRF_COOKIE_SECURE = True
+
 ALLOWED_HOSTS = ['*']
 
 
